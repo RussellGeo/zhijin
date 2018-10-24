5bc48ded742c542f75e7658e3b0dd0191fb63fe7
zhengliming <liminglives@163.com>
2018-10-24 21:22:31 +0800

    update

---
+++ zhijin-spider/spider/spiders/qdaily.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+import re
+from bs4 import BeautifulSoup
+from spider.items import NewsMeta
+from spider.utils import *
+
+class QdailyNewsSpider(scrapy.Spider):
+    name = "qdaily"
+    site = u'好奇心日报'
+    allowed_domains = ["qdaily.com"]
+    base_url = "http://m.qdaily.com"
+    start_urls = [
+        "http://m.qdaily.com/mobile/homes.html"
+    ]
+
+    def parse(self, response):
+        items = []
+        soup = BeautifulSoup(response.body, 'lxml')
+
+        clist = soup.find_all("a", class_=re.compile("swiper-slide*"))
+        for news in clist:
+            try:
+                url = news.get('href')
+                title = news.get('data-title')
+
+                item = NewsMeta()
+                item["site"] = self.site
+                item["url"] = self.base_url + url
+                item["title"] = title
+                item["datetime"] = now_datetime()
+                print item
+                items.append(item)
+            except Exception as e:
+                print e, "|||",  news
+                continue
+
+
+        clist = soup.find_all("a", class_="com-grid-article")
+        for news in clist:
+            try:
+                url = news.get('href')
+                title = news.find("h1").string
+
+                item = NewsMeta()
+                item["site"] = self.site
+                item["url"] = self.base_url + url
+                item["title"] = title
+                item["datetime"] = now_datetime()
+                print item
+                items.append(item)
+            except Exception as e:
+                print e, "|||",  news
+                continue
+        return items
+
+
+
