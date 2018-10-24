0790e12386c85fe9ecff138a29c59c7c0f237091
liming <liminglives@163.com>
2018-10-24 21:26:53 +0800

    update

---
+++ zhijin-spider/spider/spiders/rfi.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+from bs4 import BeautifulSoup
+from spider.items import NewsMeta, NewsContent
+from spider.utils import *
+from spider.mongo import mongo_helper
+
+class RfiNewsSpider(scrapy.Spider):
+    name = "rfi"
+    site = u''
+    allowed_domains = ["rfi.fr"]
+    base_url = "http://m.cn.rfi.fr"
+    start_urls = [
+            "http://m.cn.rfi.fr/"
+    ]
+
+    hheaders = {
+        #"authority":"www.voachinese.com",
+        #"method":"GET",
+        #"path":"/search_product.htm?sort=d&q=apple",
+        #"scheme":"https",
+        "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "accept-encoding":"gzip, deflate, sdch, br",
+        #"accept-language":"zh-CN,zh;q=0.8",
+        "upgrade-insecure-requests":"1",
+        "Cache-control":"max-age=0",
+        #"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        "user-agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Mobile Safari/537.36",
+        "referer":"http://m.cn.rfi.fr/",
+    }
+
+    def parse(self, response):
+        soup = BeautifulSoup(response.body, 'lxml')
+        news_list = soup.find_all("h3", class_="desc")
+        items = []
+        for news in news_list:
+            try:
+                a = news.find("a")
+                print "href", a.get('href'), type(a.get('href'))
+                item = NewsMeta()
+                item["site"] = self.site
+                item["url"] = self.base_url + a.get('href')
+                item["title"] = a.string
+                item["datetime"] = now_datetime()
+
+                print item
+                #items.append(item)
+
+                req = scrapy.Request(url = item["url"], headers = self.hheaders, callback=self.parse_content)
+                req.meta['url'] = item['url']
+
+                yield req
+            except Exception as e:
+                print e, "|||",  news
+                continue
+        #return items
+
+    def parse_content(self, response):
+        print "parse content"
+        soup = BeautifulSoup(response.body, 'lxml')
+        try:
+            content = soup.find("div", class_="bd")
+
+            news = NewsContent()
+            news['content'] = content.string
+            news['url'] = response.meta['url']
+
+            ret.append(news)
+        except Exception as e:
+            print e, "|||", response.body
+
+        return ret
+        pass
+
+
+
